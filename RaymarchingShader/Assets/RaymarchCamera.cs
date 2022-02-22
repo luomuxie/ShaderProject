b631339c3b5d4f456d5da156d59730bd323c5c18
luomuxie <835657341@qq.com>
2022-02-22 12:08:26 +0800

    将Camera视锥信息以COLOR的形式渲染

--- RaymarchingShader/Assets/RaymarchCamera.cs
+++ RaymarchingShader/Assets/RaymarchCamera.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class RaymarchCamera : MonoBehaviour
 {
-    [SerializeField]
-    private Shader shader;
+    
+    public Shader shader;
     public Material _raymarchMaterial
     {
         get
@@ -56,5 +56,52 @@ public class RaymarchCamera : MonoBehaviour
             return;
         }
         
+        _raymarchMaterial.SetMatrix("_CamFrustum", camFrustm(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+        _raymarchMaterial.SetVector ("_CamWorldSpace", _camera.transform.position);
+        RenderTexture.active = destination;
+
+        //https://docs.unity3d.com/cn/2020.2/ScriptReference/GL.LoadOrtho.html
+        GL.PushMatrix(); //将模型、视图和投影矩阵保存到矩阵堆栈顶部。
+        GL.LoadOrtho();//将正交投影加载到投影矩阵中，将标识加载到 模型和视图矩阵中。
+        _raymarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //BL
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0, 0.0f, 3.0f);
+        //BR
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1, 0.0f, 2.0f);
+        //TR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+        //TL
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+
+    }
+
+    //计算摄像机视锥底面 上下左右四个点
+    private Matrix4x4 camFrustm(Camera cam)
+    {
+        Matrix4x4 frustm = Matrix4x4.identity;
+        float fov = Mathf.Tan(cam.fieldOfView *0.5f * Mathf.Deg2Rad);//计算沿垂直方面的视野区域
+        Vector3 goUp = Vector3.up* fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;//aspect:宽高比
+        Vector3 TL = (-Vector3.forward - goRight + goUp);
+        Vector3 TR = (-Vector3.forward + goRight + goUp);
+        Vector3 BR = (-Vector3.forward + goRight - goUp);
+        Vector3 BL = (-Vector3.forward-goRight-goUp);
+        
+        frustm.SetRow(0,TL);
+        frustm.SetRow(1, TR);
+        frustm.SetRow(2, BR);
+        frustm.SetRow(3, BL);
+        
+        return frustm;
     }
 }
--- RaymarchingShader/Assets/RaymarchShader.shader
+++ RaymarchingShader/Assets/RaymarchShader.shader
@@ -19,7 +19,7 @@ Shader "Comstom/RaymarchShader"
             #include "UnityCG.cginc"
 
             sampler2D _MainTex;
-            uniform float4 _CameraWorldSpace;
+            uniform float4 _CamWorldSpace;
             uniform float4x4 _CamFrustum,_CamToWorld;
 
             struct appdata
@@ -32,13 +32,19 @@ Shader "Comstom/RaymarchShader"
             {
                 float2 uv : TEXCOORD0;
                 float4 vertex : SV_POSITION;
+                float3 ray:TEXCOORD1;
             };
 
             v2f vert (appdata v)
             {
                 v2f o;
+                half index = v.vertex.z;
+                v.vertex.z = 0;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = v.uv;
+                o.ray = _CamFrustum[(int) index].xyz;
+                o.ray /= abs(o.ray.z);
+                o.ray = mul(_CamToWorld,o.ray);
                 return o;
             }
 
@@ -46,10 +52,9 @@ Shader "Comstom/RaymarchShader"
 
             fixed4 frag (v2f i) : SV_Target
             {
-                fixed4 col = tex2D(_MainTex, i.uv);
-                // just invert the colors
-                col.rgb = 1 - col.rgb;
-                return col;
+                float3 rayDir = normalize(i.ray.xyz);
+                float3 rayOrigin = _CamWorldSpace;
+                return fixed4(rayDir,1);
             }
             ENDCG
         }
