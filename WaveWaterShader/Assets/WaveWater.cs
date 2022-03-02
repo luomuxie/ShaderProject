303da2fdcc7e1093ae219e69b8d81c37a87b764d
luomuxie <835657341@qq.com>
2022-03-02 22:46:56 +0800

    开启线程优化 ps:进一步可以使computeShader优化

--- WaveWaterShader/Assets/WaveWater.cs
+++ WaveWaterShader/Assets/WaveWater.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class WaveWater : MonoBehaviour
@@ -12,17 +13,28 @@ public class WaveWater : MonoBehaviour
     private float[,] _waveA;
     private float[,] _waveB;
     private Texture2D _texUV;
+    private bool _isRun = true;
+    private int _sleepTime;
+    private Color[] _colorBuffer;
+
     void Start()
     {
         _waveA =  new float[_waveWidth,_waveHeight];
         _waveB = new float[_waveWidth, _waveHeight];
         _texUV = new Texture2D(_waveWidth, _waveHeight);
         GetComponent<Renderer>().material.SetTexture("_WaterWave", _texUV);
+        _colorBuffer = new Color[_waveHeight*_waveHeight];
+        Thread thread = new Thread(new ThreadStart(ComPuteWave));
+        thread.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _sleepTime = (int)(Time.deltaTime * 1000);
+        _texUV.SetPixels(_colorBuffer);
+        _texUV.Apply();
+
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit; 
@@ -38,7 +50,7 @@ public class WaveWater : MonoBehaviour
             }
         }
         
-        ComPuteWave();
+        //ComPuteWave();
         
     }
 
@@ -67,39 +79,48 @@ public class WaveWater : MonoBehaviour
     //计算能量衰减及UV偏移
     void ComPuteWave()
     {
-        for (int w = 1; w < _waveHeight-1; w++)
+        while (_isRun)
         {
-            for (int h = 1; h < _waveWidth-1; h++)
+            for (int w = 1; w < _waveHeight - 1; w++)
             {
-                _waveB[w, h] = (
-                    _waveA[w - 1, h] +
-                    _waveA[w + 1, h] +
-                    _waveA[w, h - 1] +
-                    _waveA[w, h + 1] +
-                    _waveA[w - 1, h - 1] +
-                    _waveA[w - 1, h + 1] +
-                    _waveA[w + 1, h - 1] +
-                    _waveA[w + 1, h + 1])/4-_waveB[w,h];
+                for (int h = 1; h < _waveWidth - 1; h++)
+                {
+                    _waveB[w, h] = (
+                        _waveA[w - 1, h] +
+                        _waveA[w + 1, h] +
+                        _waveA[w, h - 1] +
+                        _waveA[w, h + 1] +
+                        _waveA[w - 1, h - 1] +
+                        _waveA[w - 1, h + 1] +
+                        _waveA[w + 1, h - 1] +
+                        _waveA[w + 1, h + 1]) / 4 - _waveB[w, h];
 
-                float val = _waveB[w, h];
-                if (val > 1) _waveB[w, h] = 1;
-                if(val < -1) _waveB[w,h] = -1;
-                
-                float offset_u = (_waveB[w - 1, h] - _waveB[w + 1, h]) / 2;
-                float offset_v = (_waveB[w, h-1] - _waveB[w, h+1]) / 2;
+                    float val = _waveB[w, h];
+                    if (val > 1) _waveB[w, h] = 1;
+                    if (val < -1) _waveB[w, h] = -1;
 
-                //把uv值转到颜色范围（0-1）
-                float r = offset_u / 2 + 0.5f;
-                float g = offset_v / 2 + 0.5f;
-                _texUV.SetPixel(w, h, new Color(r, g, 0));
-                _waveB[w, h] -= _waveB[w, h] * _dampVal;
+                    float offset_u = (_waveB[w - 1, h] - _waveB[w + 1, h]) / 2;
+                    float offset_v = (_waveB[w, h - 1] - _waveB[w, h + 1]) / 2;
+
+                    //把uv值转到颜色范围（0-1）
+                    float r = offset_u / 2 + 0.5f;
+                    float g = offset_v / 2 + 0.5f;
+                    _colorBuffer[w+_waveWidth*h] = new Color(r, g, 0);
+                    _waveB[w, h] -= _waveB[w, h] * _dampVal;
+                }
+
             }
 
+            float[,] temp = _waveA;
+            _waveA = _waveB;
+            _waveB = temp;
+            Thread.Sleep(_sleepTime);
         }
+       
+    }
 
-        _texUV.Apply();
-        float[,] temp = _waveA;
-        _waveA = _waveB;
-        _waveB = temp;
+    private void OnDestroy()
+    {
+        _isRun = false;
     }
 }
